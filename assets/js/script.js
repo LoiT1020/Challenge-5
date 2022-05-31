ded8c0524fc5e9d85414ba6f529717595128a2f4
LoiTran <tranloi781@gmail.com>
2022-06-01 04:05:03 +0700

    update schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,21 @@ var currenttime= moment();
 
 date.textContent= currenttime.format ("MMMM Do, YYYY - hh:mm A")
 
-/*var schedule= $()
-var time = ;
-if (moment().isAfter(time)) {
-    $(taskEl).addClass("list-group-item-secondary");
-  }
-else if (Math.abs(moment().diff(time, "hours")) =0) {
-    $(taskEl).addClass("list-group-item-danger");
-  }
-else if (Math.abs(moment().diff(time, "hours")) >1){
-    $(taskEl).addClass("list-group-item-success")
-};*/
+
+
+var schedule= $('span').text().trim();
+
+var time = moment(schedule,"hh:mm A","L");
+var attime= moment().format('hh:mm A');
+
+//via the linkhttps://stackoverflow.com/questions/23620498/comparing-two-times-with-moment-js
+var ftime= moment(time, 'hh:mma');
+var ctime=moment(attime, 'hh:mma');
+
+
+
+var yes= "you need to eat";
+if (ctime.isAfter(ftime)){
+    $("h3").addClass("list-group-item-danger");
+    console.log (yes)}
+
